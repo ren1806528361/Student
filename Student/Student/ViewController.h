29233b3502c231510c4f1edd8cb4e3b9ab4d8087
lanou3g <lanou3g@lanou3gdeMac-mini-2.local>
2015-06-23 09:46:53 +0800

    创建工程
    Signed-off-by: lanou3g <lanou3g@lanou3gdeMac-mini-2.local>

---
+++ Student/Student/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Student
+//
+//  Created by lanou3g on 15/6/23.
+//  Copyright (c) 2015年 任传荣. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Student/Student/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Student
+//
+//  Created by lanou3g on 15/6/23.
+//  Copyright (c) 2015年 任传荣. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
